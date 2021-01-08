13c70e497a254becedbd1cd0adc5164be9c385f3
Erica <erica@Ericade-iMac.local>
2021-01-08 11:41:27 +0800

    step fix

---
+++ src/scripts/slick-custom.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('#account ul, #order ul').slick({
+        centerMode: true,
+        centerPadding: '0',
+        slidesToShow: 3,
+        arrows: true,
+        responsive: [{
+            breakpoint: 640,
+            settings: {
+                centerMode: false,
+                slidesToShow: 1
+            }
+        }]
+    })
+    $(window).on('load', function(){
+        let $img = $('#account li:first-child img')
+        let h = $img.height().toFixed()
+        $('.slick-arrow').css({
+            'height': h
+        })
+    })
+  })
